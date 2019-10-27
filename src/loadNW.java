ff443aefa76c0e9021500633934a32f4090ff985
JustATechie <JustTheTechie@gmail.com>
2019-10-26 18:36:43 -0700

    Got board to print letters for columns rather than ASCII values.

--- src/loadNW.java
+++ src/loadNW.java
@@ -18,16 +18,14 @@ public class loadNW {
     }
 
     public void printBoard(){
-        int letters = 60;
+        int letters = 65;
 
         for (int k = 0; k < 10; k++){
+            char letter = (char)letters;
+            System.out.print("\t" + letter);
             letters = letters + 1;
-            System.out.print("\t" + letters);
         }
         System.out.println();
-
-        //TODO: convert unicode values into letters.
-
 
         for(int i = 0; i < 10; i++) {  //column
             System.out.print(i + 1 + "\t");
@@ -39,7 +37,15 @@ public class loadNW {
     }
 
     public void insertShip(String coordinate1, String coordinate2){
-        
+        char firstColumn = coordinate1.charAt(0);
+        int firstNumber = coordinate1.charAt(1);
+
+        char lastRow = coordinate2.charAt(0);
+        int lastNumber = coordinate2.charAt(1);
+
+        char test = (char)65;
+
+        System.out.println(test);
 
 
 
